67e0163f81ee18a6b213fd02f33e1d6103d57613
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-04-14 19:23:25 -0300

    Highlight best guess.

--- server.R
+++ server.R
@@ -12,9 +12,13 @@ source("model.R")
 shinyServer(function(input, output) {
   
   output$predicted.word <- renderUI({
-    li <- paste(paste0("<li>", predict(input$sentence), "</li>"), collapse ="" )
-    li <- paste("<ul>",li,"</ul>",sep = "")
-    HTML(li)
+    predictions <- predict(input$sentence)
+    best <- predictions[1]
+    rest <- predictions[2:length(predictions)]
+    best <- paste0("<p><b>Best guess:</b> ", best, "</p>")
+    li <- paste(paste0("<li>", rest, "</li>"), collapse ="" )
+    li <- paste("<p>Other guesses:<ul>",li,"</ul></p>",sep = "")
+    HTML(paste0(best,li))
   })
   
   output$input <- renderText({
